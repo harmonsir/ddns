f5fe0fb5e9f062d19db872be28d41bd18addd874
harmonsir <git@pylab.me>
2023-04-09 00:37:55 +0800

    fixup

--- main.py
+++ main.py
@@ -23,39 +23,42 @@ def update_dns(provider, domain, ip):
 
 
 async def update_domains(config):
-    interval = config.get("globals", {}).get("interval", 300)
-    while True:
-        for domain_config in config['domains']:
-            domain = domain_config['name']
-            # interval = domain_config['interval']
-            ip_type = domain_config['ip_type']
-            ip_urls = domain_config['ip_urls']
-            providers = domain_config['provider']
-            current_ip = get_current_ip(ip_urls)
-            if current_ip is not None:
-                for provider in providers:
-                    provider_name = provider['name']
-                    api_key = provider['api_key']
-                    zone_id = provider['zone_id']
-                    # Authenticate with provider using api_key and zone_id
-                    # Update DNS record for domain using provider and current_ip
-                    cloudflare.update_dns_record(
-                        api_key=api_key,
-                        zone_id=zone_id,
-                        domain=domain,
-                        ip_address=current_ip,
-                    )
-        print("Done and sleep 300s !!")
-        await asyncio.sleep(interval)
+    for domain_config in config['domains']:
+        domain = domain_config['name']
+        # interval = domain_config['interval']
+        ip_type = domain_config['ip_type']
+        ip_urls = domain_config['ip_urls']
+        providers = domain_config['provider']
+        current_ip = get_current_ip(ip_urls)
+        if current_ip is not None:
+            for provider in providers:
+                provider_name = provider['name']
+                api_key = provider['api_key']
+                zone_id = provider['zone_id']
+                # Authenticate with provider using api_key and zone_id
+                # Update DNS record for domain using provider and current_ip
+                cloudflare.update_dns_record(
+                    api_key=api_key,
+                    zone_id=zone_id,
+                    domain=domain,
+                    ip_address=current_ip,
+                )
 
 
 async def main():
     with open('config.yml', 'r') as f:
         config = yaml.safe_load(f)
-    try:
-        await update_domains(config)
-    except requests.HTTPError:
-        await asyncio.sleep(30)
+
+    interval = config.get("globals", {}).get("interval", 300)
+    while True:
+        try:
+            await update_domains(config)
+            print("Done and sleep 300s !!")
+            await asyncio.sleep(interval)
+        except requests.exceptions.SSLError as e:
+            print(e)
+        except requests.HTTPError:
+            await asyncio.sleep(30)
 
 
 if __name__ == '__main__':
